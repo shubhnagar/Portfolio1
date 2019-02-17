325f80dfdf00ed59f75431ac02ac3aa4bf49a38a
shubhnagar <raganhbuhs2000@gmail.com>
2019-02-17 15:43:43 +0530

    heroku

---
+++ jobs/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class job(models.Model):
+    image = models.ImageField(upload_to = 'iamges/')
+
+
+    summary = models.CharField(max_length = 200)
+
+
